535ee19a09201806d770f7d5ef83394b8035db4e
VictorLonchamps <victor.longchamps@pro22.com.br>
2024-10-07 23:11:12 -0300

    fix:solved login btn

--- src/components/AlternativeButton/index.jsx
+++ src/components/AlternativeButton/index.jsx
@@ -1,15 +1,16 @@
 import Button from "../Button";
+import "./styled.scss";
 
 const AlternativeButton = (props) => {
-    return ( <div className="alternative-btn">
-        
-        <Button alternative {...props}/>
-        <span className="bite">
-            <div className="circle"></div>
-            <div className="circle"></div>
-        </span>
-        
-    </div> );
-}
- 
+  return (
+    <div className="alternative-btn">
+      <Button alternative {...props} />
+      <span className="bite">
+        <div className="circle"></div>
+        <div className="circle"></div>
+      </span>
+    </div>
+  );
+};
+
 export default AlternativeButton;
