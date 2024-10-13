3dec8939815341cbdf8f881fe5a1f3b535af5ce4
VictorLonchamps <victor.longchamps@pro22.com.br>
2024-10-13 14:14:59 -0300

    :recycle: refactor: fixing card css

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -22,7 +22,7 @@ const props = {
   ],
 };
 
-const Card = ({ item = props, btnTitle = "comprar", click, setState, state, id }) => {
+const Card = ({ item = props, title = "comprar", click, setState, state, id }) => {
 
   useEffect(() => {
     let isMounted = true;
@@ -72,7 +72,7 @@ const Card = ({ item = props, btnTitle = "comprar", click, setState, state, id }
         </p>
         <div className="card-btns">
           {QuantityComponent()}
-          <Button click={click} placeholder={btnTitle} />
+          <Button click={click} placeholder={title} />
         </div>
       </div>
     </div>
